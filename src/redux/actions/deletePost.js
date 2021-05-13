e53761896fe87b65d90424fe72b6aaa06efb3d14
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-13 15:51:15 -0300

    fix: adjust deletePost function name

--- src/redux/actions/deletePost.js
+++ src/redux/actions/deletePost.js
@@ -1,6 +1,6 @@
 import firebase from "../../firebase/config";
 
-export const getPost = (postId, fileRef) => {
+export const deletePost = (postId, fileRef) => {
   return async dispatch => {
     const post = await firebase.deletePost(postId, fileRef);
     dispatch({ type: "DELETE_POST", payload: post });
